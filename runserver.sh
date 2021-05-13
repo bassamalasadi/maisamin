33322c4ee9c74b5dae9e86ea85738bee021da25d
bassamalasadi <bassamalasadi@gmail.com>
2021-05-13 21:22:14 +0300

    9 update

--- runserver.sh
+++ runserver.sh
@@ -2,5 +2,4 @@ python manage.py collectstatic --no-input
 
 python manage.py migrate
 
-python manage.py runserver
-# gunicorn --worker-tmp--dir /dev/shm webshop.wsgi
+gunicorn --worker-tmp--dir /dev/shm webshop.wsgi
