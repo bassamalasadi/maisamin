ec47c4c08c8e833fa6b7d71dbefc529448465820
bassamalasadi <bassamalasadi@gmail.com>
2021-05-14 00:10:02 +0300

    15 update

--- webshop/settings.py
+++ webshop/settings.py
@@ -3,7 +3,7 @@ import environ
 import sys
 import dj_database_url
 import environ
-from django.core.management.utils import get_random_secret_key
+
 env = environ.Env(
     DEBUG=(bool, False)
 )
@@ -13,7 +13,7 @@ if READ_DOT_ENV_FILE:
     environ.Env.read_env()
 
 DEBUG = env('DEBUG')
-SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
+SECRET_KEY = env('SECRET_KEY')
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
