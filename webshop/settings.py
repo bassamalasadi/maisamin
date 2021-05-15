b154e7aba2193e7eedde18ecf4b175a530fa11e1
bassamalasadi <bassamalasadi@gmail.com>
2021-05-15 09:42:11 +0300

    26 update

--- webshop/settings.py
+++ webshop/settings.py
@@ -23,8 +23,8 @@ DEBUG = os.getenv("DEBUG", "False") == "True"
 #SECRET_KEY = env('SECRET_KEY')
 SECRET_KEY = os.getenv("SECRET_KEY", get_random_secret_key())
 
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
-
+# ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
