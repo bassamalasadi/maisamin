64fcf83d4a7aa793b9cfd44b7b2530bc85ccf099
bassamalasadi <bassamalasadi@gmail.com>
2021-05-19 18:59:03 +0300

    4 final

--- webshop/settings.py
+++ webshop/settings.py
@@ -171,8 +171,8 @@ if not DEBUG:
     EMAIL_HOST = os.getenv("EMAIL_HOST")
     EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
     EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
-    EMAIL_USE_TLS = False
-    EMAIL_USE_SSL = True
+    EMAIL_USE_TLS = True
+    # EMAIL_USE_SSL = True
     EMAIL_PORT = os.getenv("EMAIL_PORT")
 
 
