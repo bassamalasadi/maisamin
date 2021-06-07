be7a74763c4dde827bdcc6568b31656a780c0f7b
bassamalasadi <bassamalasadi@gmail.com>
2021-06-07 22:00:44 +0300

    3 may

--- webshop/settings.py
+++ webshop/settings.py
@@ -16,12 +16,12 @@ if READ_DOT_ENV_FILE:
     environ.Env.read_env()
     env_var = os.environ
 
-# DEBUG = os.getenv("DEBUG", "False") == "True"
-DEBUG = False
+DEBUG = os.getenv("DEBUG", "False") == "True"
+# DEBUG = False
 
 SECRET_KEY = os.getenv("SECRET_KEY", get_random_secret_key())
 
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1, localhost").split(",")
 
 # Application definition
 
