ac1d4a96d70b05a38d08d8a19346305213a05d43
bassamalasadi <bassamalasadi@gmail.com>
2021-06-07 22:02:24 +0300

    4 may

--- webshop/settings.py
+++ webshop/settings.py
@@ -21,7 +21,8 @@ DEBUG = os.getenv("DEBUG", "False") == "True"
 
 SECRET_KEY = os.getenv("SECRET_KEY", get_random_secret_key())
 
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1, localhost").split(",")
+# ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1, localhost").split(",")
+ALLOWED_HOSTS = ['www.maisaminherkku.fi']
 
 # Application definition
 
