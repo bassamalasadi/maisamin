d83e65f111054e18ab1dde62f33fdc1af12c0b99
bassamalasadi <bassamalasadi@gmail.com>
2021-06-09 15:01:01 +0300

    1 june

--- webshop/settings.py
+++ webshop/settings.py
@@ -151,10 +151,14 @@ SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 
 # Static files (CSS, JavaScript, Images)
 
+
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = "media"
+STATIC_ROOT = "static_root"
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # STATIC_URL = '/static/'
 # MEDIA_URL = '/media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -200,12 +204,20 @@ DEFAULT_FROM_EMAIL=os.getenv("DEFAULT_FROM_EMAIL")
 
 
 
-
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'WARNING',
+    },
+}
 
 # crispy-forms for (Login and SignUp forms)
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
-
-# CORS_ALLOWED_ORIGINS = [
-#     'http://localhost:3000',
-# ]
