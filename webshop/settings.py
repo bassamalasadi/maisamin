b871a1713baf5073fadb4babd35eb5fe9a54b644
bassamalasadi <bassamalasadi@gmail.com>
2021-10-16 14:34:38 +0300

    16 oct: update domain alias

--- webshop/settings.py
+++ webshop/settings.py
@@ -21,7 +21,7 @@ DEBUG = os.getenv("DEBUG", "False") == "True"
 SECRET_KEY = os.getenv("SECRET_KEY", get_random_secret_key())
 
 # ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1, localhost").split(",")
-ALLOWED_HOSTS = ['www.maisaminherkku.fi']
+ALLOWED_HOSTS = ['www.maisaminherkku.fi', 'maisaminherkku.fi']
 
 # Application definition
 
